84f6e49088817c087632df97ce000e12a7afddbc
Thomas O'Connor <thomas.Ocono@gmail.com>
2025-05-06 20:48:16 -0400

    seems to be working

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var express = require("express");
 var cors = require("cors");
+const multer = require("multer");
 require("dotenv").config();
 
 var app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "./files");
+  },
+  filename: (req, file, callback) => {
+    callback(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: fileStorage });
 
 app.use(cors());
 app.use("/public", express.static(process.cwd() + "/public"));
@@ -11,8 +23,15 @@ app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post("/api/fileanalyse", (req, res) => {
-  res.send("hello");
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  console.log(req.file);
+  console.log("file upload success!");
+  let response = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  };
+  res.send(response);
 });
 
 const port = process.env.PORT || 3000;
